7b6a4dfdb2ba002d6ffac90563113ef8a685b693
baimiao <bm_i@163.com>
2019-02-14 08:45:34 +0800

    inital pro

---
+++ NeteaseMusic/src/pages/discover/indexPage.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: i白描
+ * @Date:   2019-02-13 18:49:18
+ * @Last Modified by:   i白描
+ * @Last Modified time: 2019-02-13 20:38:01
+ */
+
+
+import React, {
+	Component
+} from 'react'
+
+import {
+	connect
+} from 'dva';
+
+import styles from './indexPage.scss';
+
+class Baimiao extends Component {
+	render() {
+		return (
+			<div className={styles.discoverContainer}>
+				<div className='header'>
+				</div>
+			</div>
+		)
+	}
+}
+export default Baimiao
