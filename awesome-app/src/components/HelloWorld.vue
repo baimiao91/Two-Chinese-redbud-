357ecb9e9345bafc8d64464724421a14c4bc982e
baimiao <bm_i@163.com>
2019-03-01 19:53:58 +0800

    weex-start

---
+++ awesome-app/src/components/HelloWorld.vue
@@ -0,0 +1,3 @@
+<template>
+  <text class="message" style="text-align: center;">现在可以开始使用weex开发出一个helloword的组件了</text>
+</template>
---
+++ awesome-app/src/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+    <image :src="logo" class="logo" />
+    <text class="greeting">环境已经准备好了!</text>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      logo: 'http://123.206.55.50:11000/static/9d6dcac7f12b2987996fcc15.jpg'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+  .logo {
+    width: 424px;
+    height: 200px;
+  }
+  .greeting {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
